35bac8bc67a5052a5ce49cfb0a18415da56707bc
tataurova <tataurovaoe@yandex.ru>
2019-10-20 23:38:37 +0300

    Передача данных на сервер

--- js/backend.js
+++ js/backend.js
@@ -20,4 +20,25 @@
     xhr.open('GET', URL);
     xhr.send();
   };
+
+  window.upload = function (data, onSuccess, onError) {
+    var URL = 'https://js.dump.academy/keksobooking';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
 })();
